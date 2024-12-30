453749570ebf9f6ce1351e2ea1b072d6f41556a7
cristian arando <crisarandosyse@gmail.com>
2024-12-30 13:56:37 -0400

    feat: :sparkles: add CORS support and update routing structure

---
+++ src/config/cors.ts
@@ -0,0 +1,13 @@
+import { CorsOptions } from "cors";
+
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const whiteList = [];
+    whiteList.push(process.env.FRONTEND_URL);
+
+    if (process.argv[2] === "--api") whiteList.push(undefined); // npm run dev --api
+
+    if (whiteList.includes(origin)) callback(null, true);
+    else callback(new Error("Not allowed by CORS"));
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import router from "./router";
+import { corsConfig } from "./config/cors";
+import cors from "cors";
 
 const app = express();
 
-app.use("/", router);
+app.use(cors(corsConfig));
+
+app.use("/api", router);
 
 export default app;
