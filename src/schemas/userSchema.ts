2229578645310851ef138fb9062613868902ff52
cristian arando <crisarandosyse@gmail.com>
2025-01-01 01:25:43 -0400

    feat: :sparkles: update userSchema to limit handle length to a maximum of 30 characters

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const userSchema = z.object({
-  handle: z.string().min(3).max(255),
+  handle: z.string().min(3).max(30),
   name: z.string().min(3).max(255),
   email: z.string().email(),
   password: z.string().min(8).max(255),
