1033f453f56e1ccc5337610e2f57e8117a16a08f
cristian arando <crisarandosyse@gmail.com>
2025-01-03 14:56:02 -0400

    fix: :bug: update FRONTEND_URL in .env.docker for production and add localhost to CORS whitelist

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,6 +4,7 @@ export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [];
     whiteList.push(process.env.FRONTEND_URL);
+    whiteList.push("http://localhost:5173");
 
     if (process.argv[2] === "--api") whiteList.push(undefined); // npm run dev --api
 
